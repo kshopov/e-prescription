0b8bcc0c0e97ff19f09f96470195d888eeb1cc72
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-03-19 12:22:15 +0200

    created registration action

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,29 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
-{
-	public function index()
-	{
-	    echo view('header');
-            echo view('/forms/login_form');
-	    echo view('footer');
-	}
+class Home extends BaseController {
+
+    private $validation;
+
+    public function __construct() {
+        helper(['form']);
+        $this->validation = \Config\Services::validation();
+    }
+
+    public function index() {
+        $data = [];
+        
+        echo view('templates/header', $data);
+        echo view('/forms/login_form', $data);
+        echo view('templates/footer');
+    }
+
+    public function register() {
+        $data = [];
+        
+        echo view('templates/header', $data);
+        echo view('/forms/registration_form', $data);
+        echo view('templates/footer');
+    }
+
 }
