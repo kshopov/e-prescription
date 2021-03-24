cf3d264d461445819b6de383c0686d044dd1109e
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-03-24 12:05:31 +0200

    added controller for eprescpriotn

--- app/Controllers/EPrescription.php
+++ app/Controllers/EPrescription.php
@@ -1,10 +1,12 @@
 <?php
 
-
 namespace App\Controllers;
 
-
-class Eprescription
+class EPrescription extends BaseController
 {
+    public function index() {
+        echo view('templates/header');
+        echo view('templates/footer');
+    }
 
 }
