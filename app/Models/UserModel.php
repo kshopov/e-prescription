9b942ffa7436c68602f3e8c1ab54b81761c2753c
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-03-28 10:38:27 +0300

    changed table name from users to doctors

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 
 class UserModel extends Model {
 
-    protected $table = 'users';
+    protected $table = 'doctors';
     protected $primaryKey = 'id';
 
     protected $allowedFields = ['email', 'uin', 'rcz', 'password'];
