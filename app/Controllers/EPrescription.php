c7044813f95dbc0d57186900b513510627b521b5
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-04-01 13:37:16 +0300

    adds form validation

--- app/Controllers/EPrescription.php
+++ app/Controllers/EPrescription.php
@@ -9,8 +9,18 @@ class EPrescription extends BaseController
     }
     
     public function index() {
+        $data = [];
+        
+        if($this->request->getMethod() == 'post') {
+            if(!$this->validate('patientRules')) {
+                $data['validation'] = $this->validator;
+            } 
+        } else {
+            
+        }
+        
         echo view('templates/header');
-        echo view('forms/prescription_form');
+        echo view('forms/prescription_form', $data);
         echo view('templates/footer');
     }
 }
