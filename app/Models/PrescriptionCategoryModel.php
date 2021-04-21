15de9e82eb51c4c4fdbdce8b3d745267a3a5e751
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-04-21 12:07:23 +0300

    creates model for prescription category table

--- app/Models/PrescriptionCategoryModel.php
+++ app/Models/PrescriptionCategoryModel.php
@@ -1,10 +1,16 @@
 <?php
 
-
 namespace App\Models;
 
+use CodeIgniter\Model;
 
-class PrescriptionCategoryModel
-{
+class PrescriptionCategoryModel extends Model {
 
+    public static $CATEGORY_WHITE = 1;
+
+    public static $CATEGORY_REIMBURSED_5 = 2;
+
+    public static $CATEGORY_REIMBURSED_5A = 3;
+
+    protected $table = 'PRESCRIPTION_CATEGOTY';
 }
