ecec57a649095e168c30bf9929f1ee65bcc89299
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-04-21 12:06:41 +0300

    creates model for dispansation type table

--- app/Models/DispansationTypeModel.php
+++ app/Models/DispansationTypeModel.php
@@ -1,10 +1,19 @@
 <?php
 
-
 namespace App\Models;
 
+use CodeIgniter\Model;
 
-class DIspansationTypeModel
-{
+class DispansationTypeModel extends Model {
+
+    public static $SINGLE_USE = 1;
+
+    public static $MULTIPLE_USE = 2;
+
+    protected  $table = 'DISPANSATION_TYPE';
+
+    public static function getDispansationType($type) {
+        return $type == 1 ? DispansationTypeModel::$MULTIPLE_USE : DispansationTypeModel::$SINGLE_USE;
+    }
 
 }
