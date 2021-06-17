bc01305e3d36367b4fc0511971ff5fc430a3cd0a
Krasimir Shopov <krasimir.shopov@gmail.com>
2021-06-17 14:33:34 +0300

    added some mail configurations

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -33,7 +33,7 @@ class Email extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $protocol = 'mail';
+	public $protocol = 'smtp';
 
 	/**
 	 * The server path to Sendmail.
@@ -47,7 +47,7 @@ class Email extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $SMTPHost;
+	public $SMTPHost = 'localhost';
 
 	/**
 	 * SMTP Username
@@ -110,7 +110,7 @@ class Email extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $mailType = 'text';
+	public $mailType = 'html';
 
 	/**
 	 * Character set (utf-8, iso-8859-1, etc.)
